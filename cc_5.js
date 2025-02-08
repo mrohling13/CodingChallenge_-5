0db2c58d2610784e00bec84e95991a3431162dde
mrohling13 <malachi.rohling@yahoo.com>
2025-02-08 00:44:22 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,7 @@
+// Task 1: Object Properties
+const customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+};
+console.log(`Name: ${customer.name}, Age: ${customer.age}, Email: ${customer.email}`);
